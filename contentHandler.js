b6f509c5aed58c993a8100da6e0796d83dce8282
Glitch (frill-corn) <none>
2020-09-06 05:48:21 +0000

    somehow undoed someone elses pull request

--- contentHandler.js
+++ contentHandler.js
@@ -6,12 +6,16 @@ function retrieveStream(identifier) {
     identifier.startsWith("http://youtube.com") ||
     identifier.startsWith("youtube.com") ||
     identifier.startsWith("https://www.youtube.com") ||
-    identifier.startsWith("http://www.youtube.com")  ||
-    identifier.startsWith("https://youtu.be") ||
-    identifier.startsWith("http://youtu.be")
+    identifier.startsWith("http://www.youtube.com") ||
+    identifier.startsWith("http://youtu.be") ||
+    identifier.startsWith("https://youtu.be")
   ) {
     //return ytdl(identifier, { filter: 'audioonly'});
-    return ytdl(identifier, { filter: format => format.container === "mp4", highWaterMark: config.inputChunkSize, end: false}); // End: false required to prevent closing
+    return ytdl(identifier, {
+      filter: format => format.container === "mp4",
+      highWaterMark: config.inputChunkSize,
+      end: false
+    }); // End: false required to prevent closing
   }
 }
 module.exports = { retrieveStream: retrieveStream };
